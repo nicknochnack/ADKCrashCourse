b4ea9a8558a47162012a1d36ca26cfb3ea8e42e6
Nicholas Renotte <nicholas.renotte@gmail.com>
2026-05-22 09:12:07 +1000

    initcommit

---
+++ tools/stockprice.py
@@ -0,0 +1,9 @@
+from ibm_watsonx_orchestrate.agent_builder.tools import tool, ToolPermission
+import yfinance as yf
+
+@tool(name='get_stock_price',description='a tool that returns the last stock price given a stock ticker', permission=ToolPermission.ADMIN)
+def stock_price(stock_ticker:str): 
+    data = yf.Ticker(stock_ticker)
+    prices = data.history(period='1mo')
+    return prices['Close'].iloc[-1]
+
